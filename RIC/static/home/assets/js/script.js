d4a15e5dcb692f13d2ec9f56fffc3b4d1fb13f39
Chetan Chinchulkar <76626599+Chetan-Chinchulkar@users.noreply.github.com>
2023-02-02 01:20:00 +0530

    backend integrated with frontedn

--- RIC/static/home/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-
-let x = 1;
-images = ['iitg.jpg','iitg4.jpg','iitg6.jpg'];
-setInterval(() => {
-    document.getElementById('header-img').src = `/static/assets/${images[x]}`;
-    x++; 
-    x %= images.length
-}, 3000);
